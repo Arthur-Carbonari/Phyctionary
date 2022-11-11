829c37e9bd5909442340a225419b26c8691fc9b5
Arthur Martins <arthurcarbonari99@gmail.com>
2022-11-11 20:10:11 +0000

    Implement init_ui

--- code/WelcomeDialog.py
+++ code/WelcomeDialog.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton, QHBoxLayout
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QFormLayout, QLineEdit
 
 
 class WelcomeDialog(QDialog):
@@ -12,24 +13,40 @@ class WelcomeDialog(QDialog):
         self.setWindowTitle("Phyctionary")
         self.setModal(True)
 
+        self.player_one = QLineEdit(self)
+        self.player_two = QLineEdit(self)
+
+        self.init_ui()
+
+        self.exec()
+
+    def init_ui(self):
         layout = QVBoxLayout()
 
         layout.addWidget(QLabel("Welcome to our game\nPlease select your desired game mode:"))
+
+        layout_one = QFormLayout()
+        layout_one.addRow(QLabel("Player 1: "), self.player_one)
+        layout.addLayout(layout_one)
+
+        layout_two = QFormLayout()
+        layout_two.addRow(QLabel("Player 2: "), self.player_two)
+        layout.addLayout(layout_two)
 
         button_box = QHBoxLayout()
 
         easy_button = QPushButton("Easy")
         button_box.addWidget(easy_button)
-        easy_button.clicked.connect(lambda: self.set_choice("easy"))
 
         hard_button = QPushButton("Hard")
         button_box.addWidget(hard_button)
+
+        # Connect signals to slots
+        easy_button.clicked.connect(lambda: self.set_choice("easy"))
         hard_button.clicked.connect(lambda: self.set_choice("hard"))
 
         layout.addLayout(button_box)
         self.setLayout(layout)
-
-        self.exec()
 
     def set_choice(self, choice):
         self.parent.get_list(choice)
