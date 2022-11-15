ae7fdee96ebe93e361a086d497b6234ff533dc73
Arthur Martins <arthurcarbonari99@gmail.com>
2022-11-15 20:36:15 +0000

    Create GuessBox class

---
+++ code/GuessBox.py
@@ -0,0 +1,20 @@
+from PyQt6.QtWidgets import QWidget, QStyleOption, QStyle, QFrame, QLineEdit, QVBoxLayout, QTextEdit
+
+
+class GuessBox(QFrame):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setStyleSheet("""
+                    background-color: white;
+                    border-radius: 8px;
+                    border: 1px solid #000;
+        """)
+
+    # def paintEvent(self, e):
+    #     opt = QStyleOption()
+    #     opt.initFrom(self)
+    #     painter = QPainter(self)
+    #     self.style().drawPrimitive(QStyle.PrimitiveElement.PE_Widget, opt, painter, self)
+
