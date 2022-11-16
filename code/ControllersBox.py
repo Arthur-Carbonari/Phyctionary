951672cb977fa575830a77104778fb99f8f15e81
Arthur Martins <arthurcarbonari99@gmail.com>
2022-11-16 18:36:19 +0000

    Implement ControllersBox init_ui method

--- code/ControllersBox.py
+++ code/ControllersBox.py
@@ -1,8 +1,22 @@
-from PyQt6.QtWidgets import QFrame
+from PyQt6.QtWidgets import QFrame, QVBoxLayout, QWidget
 
 
 class ControllersBox(QFrame):
 
-    def __int__(self):
-        pass
+    def __init__(self, game):
+        super().__init__()
+        layout = QVBoxLayout(self)
+        wid = QWidget()
+        layout.addWidget(wid, 1)
+        self._init_ui()
 
+    def _init_ui(self):
+        self.setObjectName("ControllersBox")
+        self.setStyleSheet("""
+            #ControllersBox {
+                background-color: #142d4c;
+                opacity: 0,5;
+                border-radius: 8px;
+                border: 1px solid #000;
+            }
+        """)
