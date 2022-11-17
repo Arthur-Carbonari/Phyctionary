27e18714f5b75ddadcc89437aa89feb2825a7268
Arthur Martins <arthurcarbonari99@gmail.com>
2022-11-17 15:51:59 +0000

    Added Eraser tool to Canvas

--- code/Canvas.py
+++ code/Canvas.py
@@ -2,19 +2,22 @@ from PyQt6.QtCore import Qt, QPoint
 from PyQt6.QtGui import QPainter, QPixmap, QPen, QColor
 from PyQt6.QtWidgets import QWidget, QFileDialog
 
-from DrawingTool import PaintBrush, PaintSpray
+from DrawingTool import PaintBrush, PaintSpray, Eraser
 
 
 class Canvas(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.canvas_background = QPixmap()  # documentation: https://doc.qt.io/qt-6/qpixmap.html
+        self.canvas_background.fill(Qt.GlobalColor.white)
 
         self.image = QPixmap("./icons/canvas.png")  # documentation: https://doc.qt.io/qt-6/qpixmap.html
-        self.image.fill(Qt.GlobalColor.white)  # documentation: https://doc.qt.io/qt-6/qpixmap.html#fill
+        self.image.fill(Qt.GlobalColor.transparent)  # documentation: https://doc.qt.io/qt-6/qpixmap.html#fill
 
         self.tool_kit = {
             "brush": PaintBrush(self),
+            "eraser": Eraser(self),
             "spray": PaintSpray(self)
         }
 
@@ -31,6 +34,7 @@ class Canvas(QWidget):
     # Slots ========================================
 
     def change_current_tool(self, tool_name):
+        print(tool_name)
         self.current_tool = self.tool_kit[tool_name]
 
     def change_tool_size(self, px_size):
@@ -75,6 +79,7 @@ class Canvas(QWidget):
     def paintEvent(self, event):
         # you should only create and use the QPainter object in this method, it should be a local variable
         canvas_painter = QPainter(self)  # create a new QPainter object, docs: https://doc.qt.io/qt-6/qpainter.html
+        canvas_painter.drawPixmap(QPoint(), self.canvas_background)
         canvas_painter.drawPixmap(QPoint(), self.image)  # draw image, https://doc.qt.io/qt-6/qpainter.html#drawImage-1
 
     def resizeEvent(self, event):
