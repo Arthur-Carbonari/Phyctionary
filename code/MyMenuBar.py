f33efa353d04dc0d72d7189570f294b28e832b24
Arthur Martins <arthurcarbonari99@gmail.com>
2022-11-17 17:13:42 +0000

    Add Undo and Redo Action to Menu bar

--- code/MyMenuBar.py
+++ code/MyMenuBar.py
@@ -11,14 +11,14 @@ class MyMenuBar(QMenuBar):
 
         # Add menus to menu bar
         file_menu = self.addMenu(" File")  # Add file menu to the menu bar, "File" is reserved in Mac
-        brush_size_menu = self.addMenu(" Brush Size")  # Add the "Brush Size" menu to the menu bar
-        brush_color_menu = self.addMenu(" Brush Colour")  # Add the "Brush Colour" menu to the menu bar
+        tool_size_menu = self.addMenu(" Tool Size")  # Add the "Brush Size" menu to the menu bar
+        tool_color_menu = self.addMenu(" Tool Colour")  # Add the "Brush Colour" menu to the menu bar
 
         # Save Action
         save_action = QAction(QIcon("./icons/save.png"), "Save", parent)  # Create a save action with an icon
         save_action.setShortcut("Ctrl+S")  # Connect this save action to a keyboard shortcut
         file_menu.addAction(save_action)  # Add the save action to the file menu
-        save_action.triggered.connect(parent.save)  # Connect action to respective slot
+        save_action.triggered.connect(parent.canvas.save)  # Connect action to respective slot
 
         # Clear Action
         clear_action = QAction(QIcon("./icons/clear.png"), "Clear", parent)  # Create a clear action with icon
@@ -26,44 +26,33 @@ class MyMenuBar(QMenuBar):
         file_menu.addAction(clear_action)  # Add this action to the file menu
         clear_action.triggered.connect(parent.canvas.clear)  # Connect action to respective slot
 
+        # Undo action
+        undo_action = QAction(QIcon("./icons/undo.png"), "Undo", parent)  # Create a clear action with icon
+        undo_action.setShortcut("Ctrl+Z")  # Connect this clear action to a keyboard shortcut
+        file_menu.addAction(undo_action)  # Add this action to the file menu
+        undo_action.triggered.connect(parent.canvas.undo)  # Connect action to respective slot
+        
+        # Redo action
+        redo_action = QAction(QIcon("./icons/redo.png"), "Redo", parent)  # Create a clear action with icon
+        redo_action.setShortcut("Ctrl+Y")  # Connect this clear action to a keyboard shortcut
+        file_menu.addAction(redo_action)  # Add this action to the file menu
+        redo_action.triggered.connect(parent.canvas.redo)  # Connect action to respective slot
+
         # Change Brush Thickness
-        three_px_action = QAction(QIcon("./icons/three_px.png"), "3px", parent)
-        three_px_action.setShortcut("Ctrl+3")
-        brush_size_menu.addAction(three_px_action)  # connect the action to the function below
-        three_px_action.triggered.connect(lambda: parent.canvas.change_tool_size(3))
+        increase_tool_size = QAction(QIcon(), "Increase brush size", parent)
+        increase_tool_size.setShortcut("Ctrl++")
+        tool_size_menu.addAction(increase_tool_size)  # connect the action to the function below
+        increase_tool_size.triggered.connect(parent.controllers_box.increase_tool_size)
 
-        five_px_action = QAction(QIcon("./icons/five_px.png"), "5px", parent)
-        five_px_action.setShortcut("Ctrl+5")
-        brush_size_menu.addAction(five_px_action)
-        five_px_action.triggered.connect(lambda: parent.canvas.change_tool_size(5))
-
-        seven_px_action = QAction(QIcon("./icons/seven_px.png"), "7px", parent)
-        seven_px_action.setShortcut("Ctrl+7")
-        brush_size_menu.addAction(seven_px_action)
-        seven_px_action.triggered.connect(lambda: parent.canvas.change_tool_size(7))
-
-        nine_px_action = QAction(QIcon("./icons/nine_px.png"), "9px", parent)
-        nine_px_action.setShortcut("Ctrl+9")
-        brush_size_menu.addAction(nine_px_action)
-        nine_px_action.triggered.connect(lambda: parent.canvas.change_tool_size(9))
+        # Change Brush Thickness
+        decrease_tool_size = QAction(QIcon("./icons/three_px.png"), "Decrease brush size", parent)
+        decrease_tool_size.setShortcut("Ctrl+-")
+        tool_size_menu.addAction(decrease_tool_size)  # connect the action to the function below
+        decrease_tool_size.triggered.connect(parent.controllers_box.decrease_tool_size)
 
         # Change Brush Colors
         black_action = QAction(QIcon("./icons/black.png"), "Black", parent)
         black_action.setShortcut("Ctrl+B")
-        brush_color_menu.addAction(black_action)
-        black_action.triggered.connect(lambda: parent.canvas.change_tool_color("black"))
+        tool_color_menu.addAction(black_action)
+        black_action.triggered.connect(lambda: parent.controllers_box.change_current_color("#000000"))
 
-        red_action = QAction(QIcon("./icons/red.png"), "Red", parent)
-        red_action.setShortcut("Ctrl+R")
-        brush_color_menu.addAction(red_action)
-        red_action.triggered.connect(lambda: parent.canvas.change_tool_color("red"))
-
-        green_action = QAction(QIcon("./icons/green.png"), "Green", parent)
-        green_action.setShortcut("Ctrl+G")
-        brush_color_menu.addAction(green_action)
-        green_action.triggered.connect(lambda: parent.canvas.change_tool_color("green"))
-
-        yellow_action = QAction(QIcon("./icons/yellow.png"), "Yellow", parent)
-        yellow_action.setShortcut("Ctrl+Y")
-        brush_color_menu.addAction(yellow_action)
-        yellow_action.triggered.connect(lambda: parent.canvas.change_tool_color("yellow"))
