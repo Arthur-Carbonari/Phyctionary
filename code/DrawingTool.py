0d226423c2face35d47d31cf8690877c17e5c85a
Arthur Martins <arthurcarbonari99@gmail.com>
2022-11-18 17:58:42 +0000

    Create PaintBucket class that extends DrawingTool

--- code/DrawingTool.py
+++ code/DrawingTool.py
@@ -23,7 +23,7 @@ class DrawingTool:
 class PaintBrush(DrawingTool):
 
     def mouse_drag(self, event):
-        painter = QPainter(self.canvas.drawing)  # object which allows is_drawing to take place on an drawing
+        painter = QPainter(self.canvas.drawing)  # object which allows is_drawing to take place on a drawing
 
         # allows the selection of brush colour, brish size, line type, cap type, join type.
         painter.setPen(QPen(QColor(self.canvas.tool_color), self.canvas.tool_size, Qt.PenStyle.SolidLine,
@@ -82,3 +82,63 @@ class PaintSpray(DrawingTool):
             yo = random.gauss(0, self.canvas.tool_size)
 
             painter.drawPoint(int(event.position().x() + xo), int(event.position().y() + yo))
+
+
+class PaintBucket(DrawingTool):
+
+    def __init__(self, canvas):
+        super().__init__(canvas)
+
+        self.img = canvas.drawing.toImage()
+        self.h = self.img.height()
+        self.w = self.img.width()
+
+    def mouse_press(self, event):
+        self.img = self.canvas.drawing.toImage()
+
+        self.h = self.img.height()
+        self.w = self.img.width()
+
+        click_x, click_y = event.pos().x(), event.pos().y()
+
+        target_color = self.img.pixel(click_x, click_y)
+
+        painter = QPainter(self.canvas.drawing)
+        painter.setPen(QColor(self.canvas.tool_color))
+
+        check_queue = [(click_x, click_y)]
+        already_checked = set()
+
+        while check_queue:
+            current_x, current_y = check_queue.pop()
+
+            if self.img.pixel(current_x, current_y) == target_color:
+
+                # Fills the pixel with the active tool color
+                painter.drawPoint(current_x, current_y)
+
+                # Adds the surrounding pixels to the check_queue
+                check_queue = self._get_surrounding_pixels(already_checked, current_x, current_y) + check_queue
+
+        self.canvas.update()
+
+    def mouse_drag(self, event):
+        pass
+
+    def _get_surrounding_pixels(self, already_checked, center_x, center_y):
+        points = []
+
+        for x_var, y_var in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            x = center_x + x_var
+            y = center_y + y_var
+
+            if 0 <= x < self.w and 0 <= y < self.h:
+                if (x, y) not in already_checked:
+                    points.append((x, y))
+                    already_checked.add((x, y))
+                else:
+                    print("already checked")
+
+        return points
+
+
